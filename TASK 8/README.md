3e867a6073ab9ab02158b5ae740e6c38b00da28f
PrasunSh <prasundummy@gmail.com>
2024-04-08 03:21:10 +0530

    TASK8Complete

--- TASK 8/README.md
+++ TASK 8/README.md
@@ -11,4 +11,6 @@ The logic behind till now was the least radius of sphere covered by the arm woul
        |
 And the longest radius would be when the robotic arm is completely straight.
 I have considered that the angle is resticted between -90 to +90 from the previous arm.
+I have also found the initial joint coordinates using the angles.
+Also, I couldn't implement the FABRIK algorithm.
 
--- TASK 8/roboticarm.py
+++ TASK 8/roboticarm.py
@@ -26,6 +26,19 @@ class Arm:
         else:
             print("Target is not reachable.")
             return False
+        
+def convert_to_coordinates(angles):
+    x1 = 23 * math.sin(angles[0])
+    y1 = 23 * math.cos(angles[0])
+    x2 = x1 + 15 * math.sin(angles[0] + angles[1])
+    y2 = y1 + 15 * math.cos(angles[0] + angles[1])
+    x3 = x1 + x2 + 8 * math.sin(angles[0] + angles[1] + angles[2])
+    y3 = y1 + y2 + 8 * math.cos(angles[0] + angles[1] + angles[2])
+    print("The joint coordinates are:")
+    print(x1,y1,"0")
+    print(x2,y2,"0")
+    print(x2,y3,"0")
+
 
 
 angle = input("Enter the three angles separated by a space (in radians):" )
@@ -38,5 +51,7 @@ posi_array = tposi.split()
 posis = [int(x) for x in posi_array]
 arm.set_target_position(posis)
 arm.is_reachable()
+convert_to_coordinates(angles)
+
 
 
